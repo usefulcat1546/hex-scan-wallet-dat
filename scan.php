99b3d261802a79f49f2d4610384e24ef8805f1d8
usefulcat1546 <83736924+usefulcat1546@users.noreply.github.com>
2021-05-13 00:16:50 +0200

    Create scan.php

---
+++ scan.php
@@ -0,0 +1,31 @@
+<?php
+$db = file_get_contents("db.txt");
+$lenght = strlen($db);
+	while($lenght > 64) {
+		$secret = substr($db, 0, 64);
+		$count = substr_count($secret,"0");
+		shell_exec("echo ".$secret."   ".$lenght." > status.txt");
+		$getaddress = shell_exec("node node.js " . $secret);
+		$getaddress = trim(preg_replace('/\s\s+/', ' ', $getaddress));
+		if ($getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "..." || 
+				$getaddress == "...") {
+          shell_exec("touch ".$address.".json");
+					shell_exec("echo ".$secret." > ".$address.".json");
+					echo "FOUND " . $secret . PHP_EOL;
+			}
+		$db = substr($db, 2);
+		$lenght = strlen($db);
+	}
+?>
