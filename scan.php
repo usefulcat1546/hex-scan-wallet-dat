c76e966a27796f8c652e85120bbf1edd91a7fad8
usefulcat1546 <83736924+usefulcat1546@users.noreply.github.com>
2021-05-13 00:34:34 +0200

    Update scan.php

--- scan.php
+++ scan.php
@@ -6,24 +6,11 @@ $lenght = strlen($db);
 		shell_exec("echo ".$secret."   ".$lenght." > status.txt");
 		$getaddress = shell_exec("node node.js " . $secret);
 		$getaddress = trim(preg_replace('/\s\s+/', ' ', $getaddress));
-		if ($getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "..." || 
-				$getaddress == "...") {
-          shell_exec("touch ".$address.".json");
-					shell_exec("echo ".$secret." > ".$address.".json");
-					echo "FOUND " . $secret . PHP_EOL;
-			}
+		if ($getaddress == "..." || $getaddress == "..." || $getaddress == "..." || $getaddress == "..." || $getaddress == "..." || $getaddress == "...") {
+          		shell_exec("touch ".$address.".json");
+			shell_exec("echo ".$secret." > ".$address.".json");
+			echo "FOUND " . $secret . PHP_EOL;
+		}
 		$db = substr($db, 2);
 		$lenght = strlen($db);
 	}
