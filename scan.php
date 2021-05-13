0d1a7bb7a19f336d42f32848ff2fe2972776184b
usefulcat1546 <83736924+usefulcat1546@users.noreply.github.com>
2021-05-13 03:08:48 +0200

    Update scan.php

--- scan.php
+++ scan.php
@@ -4,7 +4,7 @@ $addresses = file_get_contents("addresses.txt");
 $coin = $argv[1];
 $lenght = strlen($wallet);
 	while($lenght > 64) {
-		$secret = substr($db, 0, 64);
+		$secret = substr($wallet, 0, 64);
 		shell_exec("echo ".$secret."   ".$lenght." > status.txt");
 		$getaddress = shell_exec("node node.js " . $secret . " " . $coin);
 		$getaddress = trim(preg_replace('/\s\s+/', ' ', $getaddress));
