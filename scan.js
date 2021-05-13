6429728f300888b400e208ad0de815c5498a589b
usefulcat1546 <83736924+usefulcat1546@users.noreply.github.com>
2021-05-13 02:58:02 +0200

    Update scan.js

--- scan.js
+++ scan.js
@@ -1,10 +1,17 @@
 process.on('uncaughtException', function (err) {
 });
 
-var arguments = process.argv
 var CoinKey = require('coinkey')
 
+var arguments = process.argv
 var secret = arguments[2]
-var key = new CoinKey(new Buffer(secret, 'hex'), {private: 0x9e, public: 0x1e})
+var coin = arguments[3]
+
+var key = new CoinKey(new Buffer(secret, 'hex'), {private: 0x80, public: 0x00})
+
+if (coin == "BTC") {var key = new CoinKey(new Buffer(secret, 'hex'), {private: 0x80, public: 0x00})}
+if (coin == "LTC") {var key = new CoinKey(new Buffer(secret, 'hex'), {private: 0xb0, public: 0x00})}
+if (coin == "DOGE") {var key = new CoinKey(new Buffer(secret, 'hex'), {private: 0x9e, public: 0x1e})}
+
 var address = key.publicAddress
 console.log(address)
